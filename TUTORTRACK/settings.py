5c2a309c1c9682d62813f9f5ef671cfdedfa39ce
Shreyanjain07 <152770118+Shreyanjain07@users.noreply.github.com>
2025-11-13 12:38:23 +0530

    Set DEBUG to False for production readiness

--- TUTORTRACK/settings.py
+++ TUTORTRACK/settings.py
@@ -39,7 +39,7 @@ else:
 SECRET_KEY = 'django-insecure-uy3pizrh%l1ng2qah98m-4((qzpdbzfwlv)$56vd%2x$9+y^d2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
